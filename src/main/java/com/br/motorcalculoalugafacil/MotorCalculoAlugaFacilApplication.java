477979632f749277ed505f4c76eae1eb4e7d8c89
Christian Lopes <christian.lima@ntendencia.com.br>
2024-05-22 21:15:51 -0300

    first commit

---
+++ src/main/java/com/br/motorcalculoalugafacil/MotorCalculoAlugaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.br.motorcalculoalugafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorCalculoAlugaFacilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorCalculoAlugaFacilApplication.class, args);
+    }
+
+}
