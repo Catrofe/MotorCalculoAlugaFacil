f9897a0b993aed2bcad09b8010f5e3cf2f971f7b
Christian Lopes <christian.lima@ntendencia.com.br>
2024-06-04 19:51:52 -0300

    FEAT: Finalizando calculo simples do Discount

--- src/main/java/com/br/motorcalculoalugafacil/service/implementation/DiscountCalculatePriceImpl.java
+++ src/main/java/com/br/motorcalculoalugafacil/service/implementation/DiscountCalculatePriceImpl.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public class DiscountCalculatePriceImpl implements DiscountCalculatePrice {
     @Override
-    public List<DiscountOut> calculatePrice(List<Discount> discount, Double price) {
-        return List.of();
+    public List<DiscountOut> calculatePrice(List<Discount> discounts, Double price) {
+        return discounts.stream().map(discount -> calculateDiscount(discount, price)).toList();
+    }
+
+    private DiscountOut calculateDiscount(Discount discount, Double price) {
+        Double discountValue = (price * discount.pcDiscount() / 100) + discount.vlDiscount();
+        return new DiscountOut(discount.nmDiscount(), discount.vlDiscount(), discount.pcDiscount(), discountValue);
     }
 }
